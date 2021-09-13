3f74525a3ab46bfee8948fe0843699fb1abee989
Screaminlean <screaminlean@gmail.com>
2021-09-13 22:02:40 +0100

    Added some more.

--- templates/Radzen.Blazor.Server.IdMan/Data/Services/IdentityManager/IdentityManagerService.cs
+++ templates/Radzen.Blazor.Server.IdMan/Data/Services/IdentityManager/IdentityManagerService.cs
@@ -22,14 +22,75 @@ namespace Radzen.Blazor.Server.IdMan.Data.Services
             RoleManager = rolemanager;
         }
 
+        public async Task<bool> CreateNewUserAsync(string username, string email, string passwordhash)
+        {
+            IdentityUser ExistingUser = await UserManager.FindByEmailAsync(email);
+
+            if (null == ExistingUser)
+            {
+                var User = new IdentityUser {
+                    UserName = username, Email = email};
+                var CreatedResult = await UserManager.CreateAsync(User, passwordhash);
+                return CreatedResult.Succeeded;
+            }
+
+            return false;
+
+        }
+
+        public async Task<bool> UpdateUserLockAsync(string id, bool islocked)
+        {
+            IdentityUser ExistingUser = await GetUserByIdAsync(id);
+
+            if (null == ExistingUser)
+            {
+                return false;
+            }
+            var UpdateLock = await UserManager.SetLockoutEnabledAsync(ExistingUser, islocked);
+            var UpdateExpire = await UserManager.SetLockoutEndDateAsync(ExistingUser, DateTime.Now);
+
+            return UpdateLock.Succeeded && UpdateExpire.Succeeded;
+        }
+
+        public async Task<bool> UpdateUserPassword(string id, string password)
+        {
+            IdentityUser ExistingUser = await GetUserByIdAsync(id);
+            if (null == ExistingUser)
+            {
+                return false;
+            }
+
+            var token = await UserManager.GeneratePasswordResetTokenAsync(ExistingUser);
+            var Updated = await UserManager.ResetPasswordAsync(ExistingUser, token, password);
+
+            return Updated.Succeeded;
+        }
+
+        // String overload for flexablity
+        public async Task<bool> DeleteUserAsync(string userid)
+        {
+            IdentityUser User = await GetUserByIdAsync(userid);
+            return await DeleteUserAsync(User);
+        }
+
+        public async Task<bool> DeleteUserAsync(IdentityUser user)
+        {
+            IdentityUser ExistingUser = await GetUserByIdAsync(user.Id);
+            if (ExistingUser != null)
+            {
+                var Result = await UserManager.DeleteAsync(user);
+                return Result.Succeeded;
+            }
+            return false;
+        }
+
         public async Task<List<IdentityUser>> GetAllUsersAsync()
         {
-
             List<IdentityUser> Users = await UserManager.Users.ToListAsync();
             return Users;
         }
 
-        public async Task<IdentityUser> GetUserById(string id)
+        public async Task<IdentityUser> GetUserByIdAsync(string id)
         {
             return await UserManager.FindByIdAsync(id);
         }
@@ -42,9 +103,9 @@ namespace Radzen.Blazor.Server.IdMan.Data.Services
         public async Task<bool> CreateRoleAsync(string name)
         {
             // If the role name already exists Succeeded will be false
-            var x = await RoleManager.CreateAsync(new IdentityRole(name));
+            var CreateRole = await RoleManager.CreateAsync(new IdentityRole(name));
 
-            return x.Succeeded;
+            return CreateRole.Succeeded;
         }
 
         // Delete a Role 
@@ -55,44 +116,45 @@ namespace Radzen.Blazor.Server.IdMan.Data.Services
          * - Deletes the role itself -> AspNetRoles
          * - I will keep it as private to prevent mishaps!
          */
-        private async Task<bool> RemoveRoleAsync(string name)
+        public async Task<bool> DeleteRoleAsync(string id)
         {
-            bool success = false;
-            var role = await RoleManager.FindByNameAsync(name);
-            if (role != null)
+            var Role = await RoleManager.FindByIdAsync(id);
+            if (Role != null)
             {
-                var isDeleted = await RoleManager.DeleteAsync(role);
-                success = isDeleted.Succeeded;
+                var DeleteRole = await RoleManager.DeleteAsync(Role);
+                return DeleteRole.Succeeded;
             }
-            return success;
+            return false;
         }
 
         public async Task<bool> AddUserToRoleAsync(string userName, string roleName)
         {
-            bool success = false;
-            var user = await UserManager.FindByNameAsync(userName);
-            if (user != null)
+            IdentityUser User = await UserManager.FindByNameAsync(userName);
+
+            if (User != null)
             {
-                var isAdded = await UserManager.AddToRoleAsync(user, roleName);
-                success = isAdded.Succeeded;
+                var AddToRole = await UserManager.AddToRoleAsync(User, roleName);
+                return AddToRole.Succeeded;
             }
-            return success;
+            return false;
         }
 
-        public async Task<bool> AddUserToRoleAsync(IdentityUser user, string roleName)
+        public async Task<bool> RemoveUserFromRole(string id, string role)
         {
-            bool success = false;
-            if (user != null)
-            {
-                var isAdded = await UserManager.AddToRoleAsync(user, roleName);
-                success = isAdded.Succeeded;
-            }
-            return success;
+            IdentityUser User = await GetUserByIdAsync(id);
+            var RemoveFromRole = await UserManager.RemoveFromRoleAsync(User, role);
+            return RemoveFromRole.Succeeded;
         }
 
         public async Task<IList<IdentityUser>> GetUsersInRoleAsync(string role)
         {
             return await UserManager.GetUsersInRoleAsync(role);
+        }
+
+        public async Task<bool> IsUserInRole(string id, string role)
+        {
+            IdentityUser User = await GetUserByIdAsync(id);
+            return await UserManager.IsInRoleAsync(User, role);
         }
 
     }
---
+++ templates/Radzen.Blazor.Server.IdMan/Data/Types/RetrunResult.cs
@@ -0,0 +1,11 @@
+namespace Radzen.Blazor.Server.IdMan.Data
+{
+    public class ReturnResult
+    {
+        public bool Success { get; set; }
+        public bool Cancelled { get; set; }
+
+    }
+
+}
+
